24e38091d3f110a7e6d4553413fc04bae877981e
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2020-06-08 13:52:03 +0530

    add the streamlit map

--- app.py
+++ app.py
@@ -13,15 +13,24 @@ st.markdown("This application is a streamlit dashboard use to analyze motor vehi
 def load_data(n_rows):
     data = pd.read_csv(DATA_URL, nrows= n_rows, parse_dates=[['CRASH DATE', 'CRASH TIME']])
     data.dropna(subset = ['LATITUDE', 'LONGITUDE'], inplace = True)
-    lowercase = lambda x: str(x).lower()
+    lowercase = lambda x: str(x).lower().replace(" ","_")
     data.rename(lowercase, axis='columns', inplace = True)
     data.rename(columns = {
-        'crash date_crash time' : 'date/time'
+        'crash_date_crash_time' : 'date/time'
     }, inplace = True)
+    # logging.info(str(data.columns))
     return data
 
-data = load_data(100000)
-logging.info('Loaded the dataset successfully')
+data = load_data(10000)
+# logging.info('Loaded the dataset successfully')
+
+st.header("Where are the most people injured in NYC?")
+injured_people = st.slider("Number of persons injured",0,19)
+st.map(data.query("number_of_persons_injured >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
+
+st.header("How many collisions occur during a give time of the day?")
+hour = st.slider("Hour to look at", 0, 23)
+data = data[data['date/time'].dt.hour == hour]
 
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
